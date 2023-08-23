d2175eaf793d1eed9a3fb4b1c661522cc5be4959
Vipul <vipulnayak1192@gmail.com>
2023-08-23 20:25:22 +0530

    Looping

---
+++ Looping/For.java
@@ -0,0 +1,15 @@
+package Looping;
+
+public class For {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 6; i++) {
+            System.out.println("Day " + i);
+            System.out.println("");
+            for (int j = 1; j <= 9; j++) {
+                System.out.println("" + (j + 8) + "-" + (j + 9));
+            }
+            System.out.println(" ");
+        }
+    }
+}
---
+++ Looping/While.java
@@ -0,0 +1,19 @@
+package Looping;
+
+public class While {
+    public static void main(String[] args) {
+        int i=1;
+
+        while(i<=4){
+            System.out.println("hi "+i);
+
+            i++;
+        }
+
+        do{
+            System.out.println("hi "+i);
+
+            i++;
+        }while(i<=4);
+    }
+}
